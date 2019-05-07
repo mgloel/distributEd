a6fa8dcc5c37c5c0f23b655f37fd454735dfbd85
Matthias Gloel <matthiasgloel@matthiass-air.localdomain>
2019-05-07 10:50:40 +0200

    read in sample size

--- server.R
+++ server.R
@@ -9,12 +9,12 @@ shinyServer(function(input, output) {
     ### if distribution...
     x = switch(
       input$distribution,
-      "uniform" = runif(10000),
-      "normal" = rnorm(10000),
-      "poisson" = rpois(10000, 2),
-      "gamma" = rgamma(10000, 2),
-      "geometric" = rgeom(10000, 0.2),
-      "binomial" = rbinom(10000, 5, 0.3)
+      "uniform" = runif(input$n),
+      "normal" = rnorm(input$n),
+      "poisson" = rpois(input$n, 2),
+      "gamma" = rgamma(input$n, 2),
+      "geometric" = rgeom(input$n, 0.2),
+      "binomial" = rbinom(input$n, 5, 0.3)
     )
     
 
@@ -23,7 +23,8 @@ shinyServer(function(input, output) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    hist(x, breaks = bins, main = paste(input$distribution,'distribution'), 
+         col = 'steelblue2', border = 'white')
 
   })
 
